74663d99eca62dad98cbb100c4603aa64bde8537
Marijn Heule <marijn@login2.ls5.tacc.utexas.edu>
2018-10-12 16:12:47 -0500

    compiled files

--- core/depend.mk
+++ core/depend.mk
@@ -1,27 +1,27 @@
-/Users/marijn/allsat/core/Main.o /Users/marijn/allsat/core/Main.or /Users/marijn/allsat/core/Main.od /Users/marijn/allsat/core/Main.op: /Users/marijn/allsat/core/Main.cc \
-  /Users/marijn/allsat/core/../utils/System.h \
-  /Users/marijn/allsat/core/../mtl/IntTypes.h \
-  /Users/marijn/allsat/core/../utils/ParseUtils.h \
-  /Users/marijn/allsat/core/../utils/Options.h \
-  /Users/marijn/allsat/core/../mtl/Vec.h \
-  /Users/marijn/allsat/core/../mtl/XAlloc.h \
-  /Users/marijn/allsat/core/../core/Dimacs.h \
-  /Users/marijn/allsat/core/../core/SolverTypes.h \
-  /Users/marijn/allsat/core/../mtl/Alg.h \
-  /Users/marijn/allsat/core/../mtl/Map.h \
-  /Users/marijn/allsat/core/../mtl/Alloc.h \
-  /Users/marijn/allsat/core/../core/Solver.h \
-  /Users/marijn/allsat/core/../mtl/Heap.h
-/Users/marijn/allsat/core/Solver.o /Users/marijn/allsat/core/Solver.or /Users/marijn/allsat/core/Solver.od /Users/marijn/allsat/core/Solver.op: /Users/marijn/allsat/core/Solver.cc \
-  /Users/marijn/allsat/core/../mtl/Sort.h \
-  /Users/marijn/allsat/core/../mtl/Vec.h \
-  /Users/marijn/allsat/core/../mtl/IntTypes.h \
-  /Users/marijn/allsat/core/../mtl/XAlloc.h \
-  /Users/marijn/allsat/core/../core/Solver.h \
-  /Users/marijn/allsat/core/../mtl/Heap.h \
-  /Users/marijn/allsat/core/../mtl/Alg.h \
-  /Users/marijn/allsat/core/../utils/Options.h \
-  /Users/marijn/allsat/core/../utils/ParseUtils.h \
-  /Users/marijn/allsat/core/../core/SolverTypes.h \
-  /Users/marijn/allsat/core/../mtl/Map.h \
-  /Users/marijn/allsat/core/../mtl/Alloc.h
+/home1/02055/marijn/allsat/core/Main.o /home1/02055/marijn/allsat/core/Main.or /home1/02055/marijn/allsat/core/Main.od /home1/02055/marijn/allsat/core/Main.op: /home1/02055/marijn/allsat/core/Main.cc \
+ /home1/02055/marijn/allsat/core/../utils/System.h \
+ /home1/02055/marijn/allsat/core/../mtl/IntTypes.h \
+ /home1/02055/marijn/allsat/core/../utils/ParseUtils.h \
+ /home1/02055/marijn/allsat/core/../utils/Options.h \
+ /home1/02055/marijn/allsat/core/../mtl/Vec.h \
+ /home1/02055/marijn/allsat/core/../mtl/XAlloc.h \
+ /home1/02055/marijn/allsat/core/../core/Dimacs.h \
+ /home1/02055/marijn/allsat/core/../core/SolverTypes.h \
+ /home1/02055/marijn/allsat/core/../mtl/Alg.h \
+ /home1/02055/marijn/allsat/core/../mtl/Map.h \
+ /home1/02055/marijn/allsat/core/../mtl/Alloc.h \
+ /home1/02055/marijn/allsat/core/../core/Solver.h \
+ /home1/02055/marijn/allsat/core/../mtl/Heap.h
+/home1/02055/marijn/allsat/core/Solver.o /home1/02055/marijn/allsat/core/Solver.or /home1/02055/marijn/allsat/core/Solver.od /home1/02055/marijn/allsat/core/Solver.op: /home1/02055/marijn/allsat/core/Solver.cc \
+ /home1/02055/marijn/allsat/core/../mtl/Sort.h \
+ /home1/02055/marijn/allsat/core/../mtl/Vec.h \
+ /home1/02055/marijn/allsat/core/../mtl/IntTypes.h \
+ /home1/02055/marijn/allsat/core/../mtl/XAlloc.h \
+ /home1/02055/marijn/allsat/core/../core/Solver.h \
+ /home1/02055/marijn/allsat/core/../mtl/Heap.h \
+ /home1/02055/marijn/allsat/core/../mtl/Alg.h \
+ /home1/02055/marijn/allsat/core/../utils/Options.h \
+ /home1/02055/marijn/allsat/core/../utils/ParseUtils.h \
+ /home1/02055/marijn/allsat/core/../core/SolverTypes.h \
+ /home1/02055/marijn/allsat/core/../mtl/Map.h \
+ /home1/02055/marijn/allsat/core/../mtl/Alloc.h
