84a1f36951a25c8dc25bd529c0960b9eb19d365a
marijn <marijn@heule.nl>
2018-10-12 16:10:14 -0500

    first version

---
+++ core/depend.mk
@@ -0,0 +1,27 @@
+/Users/marijn/allsat/core/Main.o /Users/marijn/allsat/core/Main.or /Users/marijn/allsat/core/Main.od /Users/marijn/allsat/core/Main.op: /Users/marijn/allsat/core/Main.cc \
+  /Users/marijn/allsat/core/../utils/System.h \
+  /Users/marijn/allsat/core/../mtl/IntTypes.h \
+  /Users/marijn/allsat/core/../utils/ParseUtils.h \
+  /Users/marijn/allsat/core/../utils/Options.h \
+  /Users/marijn/allsat/core/../mtl/Vec.h \
+  /Users/marijn/allsat/core/../mtl/XAlloc.h \
+  /Users/marijn/allsat/core/../core/Dimacs.h \
+  /Users/marijn/allsat/core/../core/SolverTypes.h \
+  /Users/marijn/allsat/core/../mtl/Alg.h \
+  /Users/marijn/allsat/core/../mtl/Map.h \
+  /Users/marijn/allsat/core/../mtl/Alloc.h \
+  /Users/marijn/allsat/core/../core/Solver.h \
+  /Users/marijn/allsat/core/../mtl/Heap.h
+/Users/marijn/allsat/core/Solver.o /Users/marijn/allsat/core/Solver.or /Users/marijn/allsat/core/Solver.od /Users/marijn/allsat/core/Solver.op: /Users/marijn/allsat/core/Solver.cc \
+  /Users/marijn/allsat/core/../mtl/Sort.h \
+  /Users/marijn/allsat/core/../mtl/Vec.h \
+  /Users/marijn/allsat/core/../mtl/IntTypes.h \
+  /Users/marijn/allsat/core/../mtl/XAlloc.h \
+  /Users/marijn/allsat/core/../core/Solver.h \
+  /Users/marijn/allsat/core/../mtl/Heap.h \
+  /Users/marijn/allsat/core/../mtl/Alg.h \
+  /Users/marijn/allsat/core/../utils/Options.h \
+  /Users/marijn/allsat/core/../utils/ParseUtils.h \
+  /Users/marijn/allsat/core/../core/SolverTypes.h \
+  /Users/marijn/allsat/core/../mtl/Map.h \
+  /Users/marijn/allsat/core/../mtl/Alloc.h
